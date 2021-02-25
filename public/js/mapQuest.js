6524d945fd30bb793a97b86ff522dde943e56508
Jiayan Yang <jiayanyang97@gmail.com>
2021-02-25 10:55:41 -0800

    turnin

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'ZeaPdmGAZasz9jFGT4SKVavWBZs1Ir1e';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [37.7749, -122.4194],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zomControl: false
+	});
+
+	L.marker([37.7749, -122.4194]).addTo(map);
 }
